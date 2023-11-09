c3ea58d3f76a76bddd5b5fdc4093f472c3bf5a16
antoh00 <otienoantoh@gmail.com>
2023-11-10 02:44:59 +0300

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch master # Your branch is up to date with 'origin/master'. # # Changes to be committed: #	modified:   hsh.exe #	modified:   main.c #

--- main.c
+++ main.c
@@ -9,16 +9,22 @@
 int main(void) {
     char *prompt = "$ ", *buffer;
     
-    size_t size = 10;
+    size_t size = 100;
+    ssize_t num_chars;
     
     while (1)
     {
         printf("%s", prompt);
-        getline(&buffer, &size, stdin);
-
+        num_chars = getline(&buffer, &size, stdin);
         
-        free(buffer); 
+        if(num_chars = -1){
+            printf("$ Closing shell....\n");
+
+            return (-1);
+        }
+        
+        
     }
-    
+    free(buffer); 
     return (0);
 }
